5b03f50c54b34e7b24467334a41ff0f8f3de0f4e
Riki Montgomery <mostriki820@gmail.com>
2017-08-11 09:19:40 -0700

    Modified html and js from previous project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,25 @@
 $(document).ready(function() {
-  $
+  $("#letter").submit(function() {
+  var firstName = $("input#firstName").val();
+  var lastName = $("input#lastName").val();
+  // var letterBody= $("input#letterBody").val();
+
+
+  $(".firstName").text(firstName);
+  $(".lastName").text(lastName);
+  // $(".finalLetter").text(letterBody);
+
+
+  $("#finalMessage").show();
+
+  event.preventDefault();
+});
+
+  $("#letter").submit(function() {
+    $("finalMessage").slideToggle(800);
+    $("#letter").slideToggle(800);
+    $(".extraText").slideToggle(800); //not sure what this does yet
+    $("h1").slideToggle(800);
+  });
+
+});
