8bee01498454bdb685f6b70f3c402aa44c0e20ad
Riki Montgomery <mostriki820@gmail.com>
2017-08-11 11:07:06 -0700

    Add functionality to macPc form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
   $("#suggester").submit(function() {
-  var firstName = $("input#firstName").val();
-  // var lastName = $("input#lastName").val();
+  // var age = parseInt($("input#age").val());
+  var artist = $("select#artist").val();
+  var macPc = $("select#macPc").val();
+  // var swallow = $("#age").val();
+  var money = parseInt($("input#money").val());
   // var letterBody= $("input#letterBody").val();
+  var firstName = $("input#firstName").val();
+
+  var courseChoice1 = " C#/.NET ";
+  var courseChoice2 = " CSS/Design ";
+  var courseChoice3 = " Java/Android ";
+  var courseChoice4 = " Ruby/Rails ";
+
+  // if (artist === 'True') {
+  //   alert('Artists equals True');
+  // }
+
+  if (macPc === 'PC') {
+    }
+
+  $("#course").empty().append(courseChoice1);
+  $("#courseChoice1").slideToggle(800);
+
+
+
 
 
   $(".firstName").text(firstName);
-  // $(".lastName").text(lastName);
-  // $(".finalLetter").text(letterBody);
-
 
   $("#finalMessage").show();
   event.preventDefault();
@@ -18,7 +37,7 @@ $(document).ready(function() {
     $("finalMessage").slideToggle(800);
     $("#suggester").slideToggle(800);
     $(".extraText").slideToggle(800); //not sure what this does yet
-    $("h1").slideToggle(800);
+    $("h1").fadeToggle(800);
   });
 
 });
