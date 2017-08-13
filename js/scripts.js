b29dafb6acdd8943ce1d348d7ba05c2edf61ec27
Riki Montgomery <mostriki820@gmail.com>
2017-08-13 15:43:33 -0700

    Resize the jumbotoron and rearrange the header.

--- js/scripts.js
+++ js/scripts.js
@@ -3,34 +3,33 @@ $(document).ready(function() {
   var age = parseInt($("input#age").val());
   var macPc = $("select#macPc").val();
   var swallow = $("select#swallow").val();
-  var money = parseInt($("input#money").val());
+  // var money = parseInt($("input#money").val());
   var firstName = $("input#firstName").val();
   var courseChoice1 = " C#/.NET ";
   var courseChoice2 = " CSS/Design ";
   var courseChoice3 = " Ruby/Rails ";
 
   if (age < 17) {
-    alert("You're never too young to learn how to program, but you don't have to decide anything today. Break everything!")
-    return;
-  } else if (age > 100) {
-    alert("You don't have much time! You should be traveling the world!")
-    return;
+    alert("You're never too young to learn how to program, but you don't have to decide anything today.")
+
+  } else if (age > 80) {
+    alert("I'm impressed! You're never to old to learn something new.")
   };
 
-  if ((macPc === 'PC' && money < 50000 && swallow === "I don't know")) {
-    alert("You're looking in the wrong industry.")
+  if ((macPc === 'PC' && swallow === "What kind of question is that?")) {
+    alert("Programming may not be for you.")
     return;
-  } else if ((macPc === 'PC' && swallow === 'African or european?' || "I don't know" && money > 50000)) {
+  } else if ((macPc === "PC" && swallow === "African of european?")) {
     $("#course1").empty().append(courseChoice1);
-    $("#courseChoice1").slideToggle(800);
+    $("#courseChoice1").fadeToggle(800);
     $("#finalMessage1").show();
-  } else if ((macPc === 'Mac' && swallow === "I don't know" && money < 50000)) {
+  } else if ((macPc === 'Mac' && swallow === "What kind of question is that?")) {
     $("#course2").empty().append(courseChoice2);
-    $("#courseChoice2").slideToggle(800);
+    $("#courseChoice2").fadeToggle(800);
     $("#finalMessage2").show();
   } else {
     $("#course3").empty().append(courseChoice3);
-    $("#courseChoice3").slideToggle(800);
+    $("#courseChoice3").fadeToggle(800);
     $("#finalMessage3").show();
   };
 
@@ -39,13 +38,14 @@ $(document).ready(function() {
   });
 
     $("#suggester").submit(function() {
-      $("finalMessage1").slideToggle(800);
-      $("finalMessage2").slideToggle(800);
-      $("finalMessage3").slideToggle(800);
-      $("#suggester").slideToggle(800);
-      $("#mainCopy").slideToggle(800);
-      $("#jumbotron").slideToggle(800);
-      $("h1").fadeToggle(800);
+      $("finalMessage1").fadeToggle(800);
+      $("finalMessage2").fadeToggle(800);
+      $("finalMessage3").fadeToggle(800);
+      $("#suggester").fadeToggle(800);
+      $("#mainCopy").fadeToggle(800);
+      // $("#jumbotron").fadeToggle(800);
+      // $("h1").fadeToggle(800);
+      // $("img").fadeToggle(800);
     });
 
 });
