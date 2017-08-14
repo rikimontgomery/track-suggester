a356a35993e13a449b21a0641cb139234617cb6e
Riki Montgomery <mostriki820@gmail.com>
2017-08-13 21:07:39 -0700

    Clean up scripts and overall organization. Add check empty fields for age and firstName values. Parse age integers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
   $("#suggester").submit(function() {
-  var age = parseInt($("input#age").val());
+    formEvaluation();
+    formFade ();
+  });
+});
+
+var checkEmptyField = function(name, value) {
+  if (value === "") {
+    alert("Did you fill out the "+name+" field?");
+    return (true);
+  } else {
+    return(false);
+  }
+}
+
+var formEvaluation = function() {
+  var age = $("input#age").val();
   var macPc = $("select#macPc").val();
   var swallow = $("select#swallow").val();
   // var money = parseInt($("input#money").val());
@@ -9,17 +24,28 @@ $(document).ready(function() {
   var courseChoice2 = " CSS/Design ";
   var courseChoice3 = " Ruby/Rails ";
 
-  if (age < 17) {
-    alert("You're never too young to learn how to program, but you don't have to decide anything today.")
+if (checkEmptyField("First Name", firstName)) {
+  return;
+};
+if (checkEmptyField("age", age)); {
+  return;
+}
 
-  } else if (age > 80) {
-    alert("I'm impressed! You're never to old to learn something new.")
-  };
+  if (parseInt(age) < 17) {
+    alert("You're never too young to learn how to program, but you don't have to decide anything today.")
+    return;
+  }
 
   if ((macPc === 'PC' && swallow === "What kind of question is that?")) {
     alert("Programming may not be for you.")
     return;
-  } else if ((macPc === "PC" && swallow === "African of european?")) {
+  }
+
+  if (parseInt(age) > 80) {
+    alert("I'm impressed! You're never to old to learn something new.")
+  }
+
+  if ((macPc === "PC" && swallow === "African of european?")) {
     $("#course1").empty().append(courseChoice1);
     $("#courseChoice1").fadeToggle(800);
     $("#finalMessage1").show();
@@ -35,17 +61,15 @@ $(document).ready(function() {
 
   $(".firstName").text(firstName);
   event.preventDefault();
-  });
+};
 
-    $("#suggester").submit(function() {
-      $("finalMessage1").fadeToggle(800);
-      $("finalMessage2").fadeToggle(800);
-      $("finalMessage3").fadeToggle(800);
-      $("#suggester").fadeToggle(800);
-      $("#mainCopy").fadeToggle(800);
-      // $("#jumbotron").fadeToggle(800);
-      // $("h1").fadeToggle(800);
-      // $("img").fadeToggle(800);
-    });
-
-});
+var formFade = function() {
+  $("finalMessage1").fadeToggle(800);
+  $("finalMessage2").fadeToggle(800);
+  $("finalMessage3").fadeToggle(800);
+  $("#suggester").fadeToggle(800);
+  $("#mainCopy").fadeToggle(800);
+  // $("#jumbotron").fadeToggle(800);
+  // $("h1").fadeToggle(800);
+  // $("img").fadeToggle(800);
+};
